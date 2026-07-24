899b245cb92e86c566eeafc5dcc864a171848bf3
jackysson willyan <jackyssonwillyan@gmail.com>
2026-07-24 13:55:08 -0300

    Update app_web.py

--- app_web.py
+++ app_web.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import numpy as np
-import firebase_admin
+
 
 from datetime import datetime, timezone
 from firebase_admin import credentials, firestore
